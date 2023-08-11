022927c6d3094b13236e0d6a3a7ff2fe51d42f16
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-08-11 18:46:21 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./config/database');
 const PORT = process.env.PORT;
